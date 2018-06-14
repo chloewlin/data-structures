ece423e8af9292a2ae2418a6d58788e17dbbe788
Chloe Lin <chloewjlin@gmail.com>
2018-06-13 23:13:10 -0700

    Complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!this.tail) {
+      this.tail = newNode;
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head) {
+      var oldHead = this.head.value;
+      if (this.head.next) {
+        this.head = this.head.next;
+        return oldHead;
+      } else {
+        this.head = null;
+        this.tail = null;
+        return oldHead;
+      }
+    } 
+    return undefined;
   };
 
   list.contains = function(target) {
+    var pointer = this.head;
+    
+    while (pointer) {
+      if (pointer.value === target) {
+        return true;
+      } else {
+        pointer = pointer.next;
+      }
+    }
+    
+    return false;
   };
 
   return list;
@@ -27,3 +58,18 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// addToTail: O(1)
+// removeHead: O(1)
+// contains: O(n)
+
+var LL = new LinkedList();
+LL.addToTail(4);
+LL.addToTail(5);
+LL.addToTail(6);
+LL.removeHead();
+
+console.log('contains: ' + LL.contains(8));
+console.log('current head: ' + LL.head);
+console.log('current tail: ' + LL.tail.value);
+console.log('removeHead(): ' + LL.removeHead());
