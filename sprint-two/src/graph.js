ebc2bf0361157480c483bb6484a5671ba8bfb657
Chloe Lin <chloewjlin@gmail.com>
2018-06-14 23:26:00 -0700

    Complete graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,67 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes[node] ? true : false;
 };
 
 // Removes a node from the graph.
+// Edge case: should remove edges before removing node.
+// If one node is removed, 
+// one of the two params of .removeEdge method would be null.
 Graph.prototype.removeNode = function(node) {
+  for (var key in this.nodes) {
+    this.removeEdge(node, key);
+  }
+  delete this.nodes[node];
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+// Returns a boolean indicating whether two specified nodes are connected.  
+// Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode][toNode] ? true : false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode][toNode] = true;
+  this.nodes[toNode][fromNode] = true;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode][toNode]; 
+  delete this.nodes[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.nodes) {
+    cb(node);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var graph = new Graph();
+graph.addNode(4);
+graph.addNode(5);
+graph.addEdge(4, 5);
+var a = graph.hasEdge(4, 5);
+console.log(a);
+console.log(graph);
+graph.removeEdge(5);
+// console.log(graph);
+
 
 
