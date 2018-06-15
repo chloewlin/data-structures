e06648ef8dd314ccb61f5af2462d0b93623b9e86
Chloe Lin <chloewjlin@gmail.com>
2018-06-15 00:37:56 -0700

    Complete set and annotate time complexity

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
+// Time Complexity: O(1)
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
+// Time Complexity: O(n)
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
+// Time Complexity: O(n)
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
